aacfca7fa73c671d31a66e61a83d16a750db9d68
Umair Khalid <126099551+IamUmairKhalid@users.noreply.github.com>
2025-12-27 22:53:23 +0500

    removed unused files

--- src/app/report/page.js
+++ src/app/report/page.js
@@ -13,7 +13,6 @@ import ContactStep from "../../modules/report/components/steps/ContactStep";
 import ReviewSubmitStep from "../../modules/report/components/steps/ReviewSubmitStep";
 import Button from "../../modules/shared/ui/Button";
 import { t } from "../../lib/i18n";
-import { mockSubmitCase } from "../../data/mockSubmit";
 import { useAuth } from "@/lib/AuthContext";
 import { useRouter } from "next/navigation";
 import { caseApi } from "@/lib/caseApi";
